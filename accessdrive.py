6e418f17612f53a998d28a79f183beef66a30ef0
Aryan <aryangupta9808@gmail.com>
2024-05-30 00:37:19 +0530

    chat history display

--- accessdrive.py
+++ accessdrive.py
@@ -8,17 +8,16 @@ from exception import CustomException
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-encoded_credentials = os.getenv("GOOGLE_DESKTOP_CREDENTIALS")
-
-decoded_credentials_json = base64.b64decode(encoded_credentials).decode("utf-8")
-
-def getdrive(folderid:str):
+def getdrive(folderid: str):
     SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
+    encoded_credentials = os.getenv("GOOGLE_DESKTOP_CREDENTIALS")
+    decoded_credentials_json = base64.b64decode(encoded_credentials).decode("utf-8")
     credentials_info = json.loads(decoded_credentials_json)
-    app_flow = InstalledAppFlow.from_client_secrets_file(
-      credentials_info, scopes=SCOPES
-  )
-    credentials = app_flow.run_local_server(port = 0)
+
+    # Use the 'token' key if it exists, otherwise use 'installed'
+    flow = InstalledAppFlow.from_client_config(credentials_info, SCOPES)
+    credentials = flow.run_local_server(port=0)
+
     drive_service = build("drive", "v3", credentials=credentials)
     folder_id = folderid
     results = (
@@ -38,6 +37,7 @@ def getdrive(folderid:str):
         os.makedirs("./data", exist_ok = True)
         with open(f"./data/{file_name}", "wb") as pdf_file:
             pdf_file.write(response)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="folder id")
--- main.py
+++ main.py
@@ -14,47 +14,47 @@ apikey = os.getenv("GOOGLE_API_KEY")
 
 path = os.getenv("PATH_TO_PYTHON_SCRIPT")
 
-def output(_role, textstr):
-    if "chat_history" not in st.session_state:
-        st.session_state["chat_history"] = []
+st.set_page_config("Chat with PDFs")
+st.header("Chat with PDF using Gemini💁")
 
-    st.session_state["chat_history"].append((_role, textstr))
+if "chat_history" not in st.session_state:
+  st.session_state["chat_history"] = []
 
-    for role, text in st.session_state["chat_history"]:
-        st.write(f"{role} {text}")
+for message in st.session_state["chat_history"]:
+  with st.chat_message(message["role"]):
+      st.markdown(message["content"])
 
+user_question = st.chat_input("Ask a Question from the PDF Files")
+if user_question:
+    st.chat_message("user").markdown(user_question)
+    st.session_state["chat_history"].append({"role": "user", "content": user_question})
+    res = subprocess.run([path, "model.py", "--prompt", user_question], capture_output = True, text=True)
+    st.chat_message("assisstant").markdown(res.stdout)
+    st.session_state["chat_history"].append({"role": "assistant", "content": res.stdout})
+voice_input = st.button(label="Voice Input")
+if voice_input:
+    user_question = voice_rec()
+    st.chat_message("user").markdown(user_question)
+    st.session_state["chat_history"].append({"role": "user", "content": user_question})
+    res = subprocess.run([path, "model.py", "--prompt", user_question], capture_output = True, text=True)
+    st.chat_message("assistant").markdown(res.stdout)
+    st.session_state["chat_history"].append({"role": "assistant", "content": res.stdout})
 
-if __name__ == "__main__":
-  st.set_page_config("Chat with PDFs")
-  st.header("Chat with PDF using Gemini💁")
-
-  user_question = st.chat_input("Ask a Question from the PDF Files")
-  voice_input = st.button(label="Voice Input")
-  if voice_input:
-      user_question = voice_rec()
-      output("You:", user_question)
-      res = subprocess.run([path, "model.py", "--prompt", user_question], capture_output = True, text=True)
-      output("RAG-Bot:", res.stdout)
-  if user_question:
-      output("You:", user_question)
-      res = subprocess.run([path, "model.py", "--prompt", user_question], capture_output = True, text=True)
-      output("RAG-Bot:", res.stdout)
-  
-  with st.sidebar:
-    st.title("Menu:")
-    pdf_docs = st.file_uploader(
-        "Upload your PDF Files and Click on the Submit & Process Button",
-        accept_multiple_files=True,
-    )
-    folderid = ''.join(st.text_input("Enter drive folder link: "))
-    if st.button("Submit & Process"):
-      with st.spinner("Processing..."):
-        if folderid:
-          try:
-            subprocess.run([path, "accessdrive.py", "--folderid", folderid], capture_output = False)
-            create_index()
-          except Exception as e:
-            raise CustomException(e, sys)
-        if pdf_docs:
-          create_index(pdf_docs)
-        st.success("Done")
+with st.sidebar:
+  st.title("Menu:")
+  pdf_docs = st.file_uploader(
+      "Upload your PDF Files and Click on the Submit & Process Button",
+      accept_multiple_files=True,
+  )
+  folderid = (st.text_input("Enter drive folder link: ")).split('/')[-1]
+  if st.button("Submit & Process"):
+    with st.spinner("Processing..."):
+      if folderid:
+        try:
+          subprocess.run([path, "accessdrive.py", "--folderid", folderid], capture_output = False)
+          create_index()
+        except Exception as e:
+          raise CustomException(e, sys)
+      elif pdf_docs:
+        create_index(pdf_docs)
+      st.success("Done")
